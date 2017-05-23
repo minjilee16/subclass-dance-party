a7369ef63eb87e7fc3be651a101ff545a5bb1342
Hack Reactor Student <student@hackreactor.com>
2017-05-22 20:19:26 -0700

    passed all test case

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,4 +1,5 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
+
   Dancer.call(this, top, left, timeBetweenSteps); 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -14,7 +15,7 @@ BlinkyDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
 
   // call the old version of step at the beginning of any call to this new version of step
-  // oldStep();
+  // oldStep();dsdfsdf
   // oldStep.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
